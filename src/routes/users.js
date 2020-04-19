5339339b8db6cec1a4d0d520d744512697665a7e
Benjamin Dominguez <bidominguez@uc.cl>
2020-04-19 19:44:17 -0400

    add user create from view

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,4 +7,29 @@ router.get('users.list', '/', async (ctx) => {
   // equiv                        {usersList: usersList}
 });
 
+router.get('users.new', '/new', async (ctx) => {
+  const user = ctx.orm.user.build();
+  await ctx.render('users/new', {
+    user,
+    submitUserPath: ctx.router.url('users.create'),
+   });
+  // equiv                        {user: user}
+});
+
+router.post('users.create', '/', async (ctx) =>{
+  const user = ctx.orm.user.build(ctx.request.body);
+  try {
+    await user.save({ fields: ['username',  'password', 'name', 'email',
+    'phone', 'address', 'rating']});
+    ctx.redirect(ctx.router.url('users.list'));
+  } catch (validationError) {
+    await ctx.render('users.new', {
+      user,
+      errors: validationError.errors,
+      submitUserPath: ctx.router.url('users.create'),
+    });
+  }
+});
+
+
 module.exports = router;
