3d6847c1cb3a2dc2ebd4e9fc7e74c4e59a142761
Diego Campos <dacampos1@uc.cl>
2020-04-20 12:26:41 -0400

    migracion user

--- src/migrations/20200419222613-users.js
+++ src/migrations/20200419222613-users.js
@@ -1,72 +1,27 @@
+
+'use strict';
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    // Order belongsTo Customer
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
     return queryInterface.addColumn(
-      'Orders', // name of Source model
-      'CustomerId', // name of the key we're adding 
-      {
-        type: Sequelize.UUID,
-        references: {
-          model: 'Customers', // name of Target model
-          key: 'id', // key in Target model that we're referencing
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-      }
-    )
-      .then(() => {
-        // Payment hasOne Order
-        return queryInterface.addColumn(
-          'Orders', // name of Target model
-          'PaymentId', // name of the key we're adding
-          {
-            type: Sequelize.UUID,
-            references: {
-              model: 'Payments', // name of Source model
-              key: 'id',
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'SET NULL',
-          }
-        );
-      })
-      .then(() => {
-        // Order hasMany Product
-        return queryInterface.addColumn(
-          'Products', // name of Target model
-          'OrderId', // name of the key we're adding
-          {
-            type: Sequelize.UUID,
-            references: {
-              model: 'Orders', // name of Source model
-              key: 'id',
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'SET NULL',
-          }
-        );
-      });
+          'users',
+          'rating',
+           Sequelize.FLOAT
+         );
   },
 
   down: (queryInterface, Sequelize) => {
-    // remove Order belongsTo Customer
-    return queryInterface.removeColumn(
-      'Orders', // name of Source model
-      'CustomerId' // key we want to remove
-    )
-      .then(() => {
-        // remove Payment hasOne Order
-        return queryInterface.removeColumn(
-          'Orders', // name of the Target model
-          'PaymentId' // key we want to remove
-        );
-      })
-      .then(() => {
-        // remove Order hasMany Product
-        return queryInterface.removeColumn(
-          'Products', // name of the Target model
-          'OrderId' // key we want to remove
-        );
-      });
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.dropTable('users');
+    */
   }
 };
