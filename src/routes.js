d2c743a1a5667fb936ede452a2d249065ab2572e
Diego Campos <dacampos1@uc.cl>
2020-04-19 23:05:20 -0400

    routes offer

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ const KoaRouter = require('koa-router');
 const hello = require('./routes/hello');
 const index = require('./routes/index');
 const objects = require('./routes/objects');
+const offers = require('./routes/offers');
 
 const router = new KoaRouter();
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/objects', objects.routes());
-
+router.use('/offers', objects.offers());
 
 module.exports = router;
