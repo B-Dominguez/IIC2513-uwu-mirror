27bffbd60ee049b98b824def44322f094588ea79
Diego Campos <dacampos1@uc.cl>
2020-04-20 01:07:51 -0400

    database

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,10 @@
 const config = {
   default: {
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // dialect: process.env.DB_DIALECT || 'postgres',
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST || '127.0.0.1',
-    username: "admin",
-    password: "123123",
-    dialect: "postgres",
-    database: "proyectoweb",
-    host: "localhost" || "127.0.0.1",
-
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || '127.0.0.1',
   },
   development: {
     extend: 'default',
