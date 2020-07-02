d570dcee78f58c45553ae2ad614499ef666f212f
Benjamin Dominguez <bidominguez@uc.cl>
2020-07-02 12:57:15 -0400

    objects methods

--- src/models/object.js
+++ src/models/object.js
@@ -1,3 +1,4 @@
+const spacePath = "https://guwuappspace1.sfo2.digitaloceanspaces.com/";
 module.exports = (sequelize, DataTypes) => {
   const object = sequelize.define('object', {
     name: DataTypes.STRING,
@@ -15,6 +16,49 @@ module.exports = (sequelize, DataTypes) => {
     object.belongsToMany(models.offer, { through: 'ObjectOffer' });
     // associations can be defined here. This method receives a models parameter.
   };
+
+  object.prototype.getImagePath = function(number) {
+    var image = null;
+    switch (number) {
+      case 1:
+        //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+        image = this.image1;
+        break;
+      case 2:
+        //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor2
+        image = this.image2;
+        break;
+      case 3:
+        //Declaraciones ejecutadas cuando el resultado de expresión coincide con valorN
+        image = this.image3;
+        break;
+      default:
+        //Declaraciones ejecutadas cuando ninguno de los valores coincide con el valor de la expresión
+        return null;
+    }
+    if (image) {
+      return spacePath+image;
+    } else{
+      return null;
+    }
+  };
+
+  object.prototype.getFirstImagePath = function() {
+    var i;
+    var fimage = null;
+    for (i = 1; i < 4; i++) {
+      fimage = this.getImagePath(i);
+      if (fimage) {
+        return fimage;
+      }
+    }
+    return "http://placehold.it/150x150";
+  };
+
+  object.prototype.parsedCreatedAt = function() {
+  return this.createdAt.getDate() + "/" + this.createdAt.getMonth() + "/" +
+  this.createdAt.getFullYear();
+};
 
   return object;
 };
